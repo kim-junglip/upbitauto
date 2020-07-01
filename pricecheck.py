6a4a4eff80bbacab3dc1cda1a603a1508d7cd6bc
kim-junglip <kimoffice0911@gmail.com>
2020-07-01 16:28:27 +0900

    2020-07-01 commit

--- pricecheck.py
+++ pricecheck.py
@@ -1,7 +1,7 @@
 import pyupbit
 
-access_key = "HyZz5dIuxdqqJROmHmKJB9aBODOpKtvxD50pXCzZ"
-secret_key = "QnGPbfTUUN2HyRLJKdyKLLrQckGKPPN27wfgMn6k"
+access_key =
+secret_key =
 
 upbit = pyupbit.Upbit(access_key, secret_key)
 print(upbit.get_balances())
