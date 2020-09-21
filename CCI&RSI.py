d5af072e06f67e9777d5205b61b079f1d62dc623
HOME-PC <kijl2002@naver.com>
2020-09-21 16:31:36 +0900

    2020-09-21

--- CCI&RSI.py
+++ CCI&RSI.py
@@ -30,16 +30,17 @@ while True :
             cci_total_yesterday_buy.append(int(cci_buy[-2]))
             time.sleep(0.1)
 
-            if cci_total_yesterday_buy[i] <= 110 < cci_total_today_buy[i] :
+            if cci_total_yesterday_buy[i] <= 50 < cci_total_today_buy[i] :
                 print("매수시작")
                 my_coin_buy = []
                 coin_list = []
                 for j in range(len(upbit.get_balances()[0])) :
                     coin_list.append(upbit.get_balances()[0][j])
                     my_coin_buy.append("KRW-" + coin_list[j].get('currency'))
-                if my_coin_buy.count(coin_buy[i]) == 0 :
-                    upbit.buy_market_order(coin_buy[i], 5000)
-                    print(coin_buy[i] + ":", "매수")
+                if my_coin_buy != "KRW-BTC" :
+                    if my_coin_buy.count(coin_buy[i]) == 0 :
+                        upbit.buy_market_order(coin_buy[i], 5000)
+                        print(coin_buy[i] + ":", "매수")
     except Exception as e :
         pass
         print("매수 에러")
@@ -51,37 +52,32 @@ while True :
         rsi_total_yesterday_sell = []
         cci_total_today_sell = []
         cci_total_yesterday_sell = []
-        for x in range(len(upbit.get_balances()[0])) :
-            
-
+        for x in range(1,len(upbit.get_balances()[0])) :
 
             df_sell.append(upbit.get_balances()[0][x])
-
-            sell_cur.append("KRW-" + df_sell[x].get('currency'))
-            sell_bal.append(df_sell[x].get('balance'))
+            sell_cur.append("KRW-" + df_sell[x-1].get('currency'))
+            sell_bal.append(df_sell[x-1].get('balance'))
             total_sell = sell_cur, sell_bal
-            time.sleep(0.1)
 
-            if x >= 1 :
-                df_coin_sell = pyupbit.get_ohlcv(sell_cur[x])
-                #print(df_coin_sell)
-                high_sell = df_coin_sell['high']
-                low_sell = df_coin_sell['low']
-                close_sell = df_coin_sell['close']
+            df_coin_sell = pyupbit.get_ohlcv(sell_cur[x-1])
+            high_sell = df_coin_sell['high']
+            low_sell = df_coin_sell['low']
+            close_sell = df_coin_sell['close']
 
-                rsi_sell = ta.RSI(close_sell, timeperiod=14)
-                rsi_total_today_sell.append(int(rsi_sell[-1]))
-                rsi_total_yesterday_sell.append(int(rsi_sell[-2]))
+            rsi_sell = ta.RSI(close_sell, timeperiod=14)
+            rsi_total_today_sell.append(int(rsi_sell[-1]))
+            rsi_total_yesterday_sell.append(int(rsi_sell[-2]))
 
-                cci_sell = ta.CCI(high_sell, low_sell, close_sell, timeperiod=20)
-                cci_total_today_sell.append(int(cci_sell[-1]))
-                cci_total_yesterday_sell.append(int(cci_sell[-2]))
+            cci_sell = ta.CCI(high_sell, low_sell, close_sell, timeperiod=20)
+            cci_total_today_sell.append(int(cci_sell[-1]))
+            cci_total_yesterday_sell.append(int(cci_sell[-2]))
 
 
-                if rsi_total_yesterday_sell[x-1] >= 70 > rsi_total_today_sell[x-1] \
-                        or cci_total_yesterday_sell[x-1] >= 100 < cci_total_today_buy[x-1] :
-                    upbit.sell_market_order(total_sell[0][x-1], total_sell[1][x-1])
-                    print(sell_cur[x - 1] + ":", "매도완료")
+            if rsi_total_yesterday_sell[x-1] >= 70 > rsi_total_today_sell[x-1] \
+                    or cci_total_yesterday_sell[x-1] >= 100 > cci_total_today_sell[x-1] \
+                    or cci_total_today_sell[x-1] < 40 :
+                upbit.sell_market_order(total_sell[0][x], total_sell[1][x])
+                print(sell_cur[x] + ":", "매도완료")
 
 
     except Exception as e :
